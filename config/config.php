0d6535ecb0813628966fcce29755324f4290f4ca
Allan Jake V. Mahinay <72182874+ajake011@users.noreply.github.com>
2021-05-28 15:53:49 +0800

    commit

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+	define('ROOT_URL', 'https://db4free.net/phpMyAdmin/index.php?route=/');
+	define('DB_HOST', 'localhost');
+	define('DB_USER', 'jekken011');
+	define('DB_PASS', 'user01111');
+	define('DB_NAME', 'mytable');
---
+++ inc/footer.php
@@ -0,0 +1,7 @@
+<footer class="mastfoot mt-auto" style="text-align: center;">
+    <div class="inner">
+      <p>Created by <a href="https://github.com/ajake011">ajake011</a> a BSIT 3B1</p>
+    </div>
+</footer>
+  </body>
+</html>
